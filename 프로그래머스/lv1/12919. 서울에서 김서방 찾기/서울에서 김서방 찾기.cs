71cd0015e387af1cfa5623b9dcf0c972a01ecaf2
DoHyeonKong <132756641+KKongDo@users.noreply.github.com>
2023-06-09 00:05:35 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.32 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -0,0 +1,13 @@
+using System;
+public class Solution {
+    public string solution(string[] seoul) {
+        string answer = "";
+        for(int i=0; i<seoul.Length; i++)
+        {
+            if(seoul[i]=="Kim"){
+                answer="김서방은 "+i+"에 있다";
+            }
+        }
+        return answer;
+    }
+}
