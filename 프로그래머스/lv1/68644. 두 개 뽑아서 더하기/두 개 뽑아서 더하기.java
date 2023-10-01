8ce4de7b4ed1ac7e0a3d35d3ea15ff84a49d7a95
DoHyeonKong <132756641+KKongDo@users.noreply.github.com>
2023-10-01 16:54:25 +0900

    Update 두 개 뽑아서 더하기.java

--- 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -4,7 +4,7 @@ class Solution {
     public int[] solution(int[] numbers) {
         Set<Integer> number = new HashSet<Integer>();
         for(int i = 0; i < numbers.length; i++){
-            for(int j = i +1 ; j < numbers.length; j++){
+            for(int j = i + 1 ; j < numbers.length; j++){
                 number.add(numbers[i] + numbers[j]);
             }
         }
