b328f41076ee241e96ee60184dc769ba4b66e7cf
DoHyeonKong <132756641+kkongdo@users.noreply.github.com>
2023-11-16 15:57:14 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 604 ms, Memory: 21604 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		for(int i = 1; i <= num; i++) {
+			for(int j = 0; j < num - i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i = num - 1; i > 0; i--) {
+			for(int j = 0; j < num - i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 0; j < 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
