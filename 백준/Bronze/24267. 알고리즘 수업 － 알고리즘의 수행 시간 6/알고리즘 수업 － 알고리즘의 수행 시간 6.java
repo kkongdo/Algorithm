3b7c577a225165e749f93a0e68c228ce58991ce8
DoHyeonKong <132756641+kkongdo@users.noreply.github.com>
2023-12-05 12:37:59 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Long num = Long.parseLong(br.readLine());
+		System.out.println( (num * (num - 1) * (num - 2)) / 6);
+		System.out.println(3);
+	}
+}
