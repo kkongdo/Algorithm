f78c1c81918947f5dbce247dfe7cb1639125455f
DoHyeonKong <132756641+kkongdo@users.noreply.github.com>
2023-12-05 12:18:55 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 120 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -6,11 +6,8 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Long num = Long.parseLong(br.readLine());
-		Long sum = (long) 0;
-		for(int i = 1; i < num; i++) {
-			sum += i;
-		}
-		System.out.println(sum);
+		
+		System.out.println( num * (num - 1) / 2);
 		System.out.println(2);
 	}
 }
