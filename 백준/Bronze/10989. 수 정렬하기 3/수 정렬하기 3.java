16d713c0a090a55bc44ef2f0e1a3b5533723de56
DoHyeonKong <132756641+kkongdo@users.noreply.github.com>
2023-12-07 14:32:08 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1684 ms, Memory: 336584 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//counting 함수를 이용하였다.
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] count = new int[10001];
+		
+		for(int i = 0; i < N; i++) {
+			count[Integer.parseInt(br.readLine())]++;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i < 10001; i++) {
+			while(count[i] > 0) {
+				sb.append(i).append("\n");
+				count[i]--;
+			}
+		}
+		System.out.println(sb);
+		
+	}
+}
