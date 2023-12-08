5be52013d07e797ba788587bee2308841d59dbd9
DoHyeonKong <132756641+kkongdo@users.noreply.github.com>
2023-12-08 14:16:39 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1500 ms, Memory: 56172 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		int[][] nums = new int[N][2];
+		
+		for(int i = 0;  i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			nums[i][1] = Integer.parseInt(st.nextToken());
+			nums[i][0] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(nums, (e1, e2) -> {
+			if(e1[0] == e2[0]) {
+				return e1[1] - e2[1];
+			}
+			else {
+				return e1[0] - e2[0];
+			}
+		});
+		for(int i = 0; i < N; i++) {
+			System.out.println(nums[i][1] + " " + nums[i][0]);
+		}
+	}
+}
