1845dfae4801f7a4ceb3cb27074bbdd5b164d729
DoHyeonKong <132756641+kkongdo@users.noreply.github.com>
2023-12-12 14:07:02 +0900

    [Bronze I] Title: 최소공배수, Time: 196 ms, Memory: 14996 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for(int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int result = lcm(A, B);
+			System.out.println(result); 
+		}
+	}
+
+	private static int lcm(int a, int b) {
+		return (a * b) / gcd(a, b);
+	}
+
+	private static int gcd(int a, int b) {
+		if(b == 0) {
+			return a;
+		}
+		else {
+			return gcd(b, a % b);
+		}
+	}
+}
