38193585087459ff203836b39047f12db6bebd47
Dohyeon Kong <132756641+kkongdo@users.noreply.github.com>
2023-12-15 14:33:53 +0900

    [Silver IV] Title: 제로, Time: 240 ms, Memory: 23596 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int k = Integer.parseInt(br.readLine());
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		for(int i = 0; i < k; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if(num == 0 && !stack.isEmpty()) {
+				stack.pop();
+			}
+			else {
+				stack.push(num);
+			}
+		}
+		int sum = 0;
+		for(int num : stack) {
+			sum += num;
+		}
+		System.out.println(sum);
+		
+	}
+}
