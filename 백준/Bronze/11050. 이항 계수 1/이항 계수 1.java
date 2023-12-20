7305532bd38bd2b227fc7ee4b52b6ce9f428854f
Dohyeon Kong <kkong_do@naver.com>
2023-12-20 16:02:56 +0900

    [Bronze I] Title: 이항 계수 1, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,23 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		System.out.println(bc(N, K));
+	}
+
+	private static int bc(int n, int k) {
+		if(n == k || k == 0) {
+			return 1;
+		}
+		return bc(n - 1, k - 1) + bc(n - 1, k);
+	}
+}
