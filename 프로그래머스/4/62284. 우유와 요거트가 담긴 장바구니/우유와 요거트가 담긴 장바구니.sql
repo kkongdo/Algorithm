3099b4eeeb926d84d4310661ec8be73d7d47350a
Dohyeon Kong <kkong_do@naver.com>
2023-12-31 15:59:46 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT cart_id
+FROM CART_PRODUCTS
+WHERE CART_ID IN 
+(SELECT CART_ID
+FROM CART_PRODUCTS
+WHERE NAME = 'Yogurt'
+GROUP BY CART_ID
+) AND NAME = 'Milk'
+order by cart_id
