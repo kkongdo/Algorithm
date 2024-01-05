ab2eb6dd4baaee2987e7bf0bba7fa7bb47a717ab
Dohyeon Kong <kkong_do@naver.com>
2024-01-05 13:34:32 +0900

    [Silver IV] Title: 다음 소수, Time: 944 ms, Memory: 103660 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.java
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -11,6 +11,8 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			long num = Long.parseLong(br.readLine());
 			BigInteger bi = new BigInteger(String.valueOf(num));
+			//isProbalePrime() : parameter로는 certainly에 대한 값을 넘겨야함
+			//대략 10을 넘기게 되면 해당 값이 소수인지에 대한 판별률이 99.9%에 가까워진다고 함.
 			if(bi.isProbablePrime(10)) {
 				System.out.println(num);
 			}
