9dc89ccfa144e5a404e110680dbcad829ba355db
Dohyeon Kong <kkong_do@naver.com>
2024-01-05 13:31:52 +0900

    [Bronze I] Title: 최소공배수, Time: 192 ms, Memory: 15128 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//유클리드 호제법 : 최소공배수와 최대공약수를 구할 때 쓰는 대표적인 알고리즘
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,11 +17,12 @@ public class Main {
 			System.out.println(result); 
 		}
 	}
-
+	
+	//최소공배수(Least Common Multiple)
 	private static int lcm(int a, int b) {
 		return (a * b) / gcd(a, b);
 	}
-
+	//최대공약수(Greatest Common Divisor)
 	private static int gcd(int a, int b) {
 		if(b == 0) {
 			return a;
