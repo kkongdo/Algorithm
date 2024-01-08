a73099cc24f1f862cfb95002175e0bbf80eaadee
Dohyeon Kong <kkong_do@naver.com>
2024-01-08 12:43:26 +0900

    [Bronze I] Title: 약수, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -7,7 +7,9 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		int max = Integer.MIN_VALUE;
 		int min = Integer.MAX_VALUE;
 		
