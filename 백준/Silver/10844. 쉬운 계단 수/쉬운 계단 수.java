715faa5df9e7b75715421dd4b8beae8f41fdbbe5
Dohyeon Kong <kkong_do@naver.com>
2024-01-08 13:19:22 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static Long[][] dp;
+	static int N;
+	final static long MOD = 1000000000;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		dp = new Long[N + 1][10];
+		
+		for(int i = 0; i < 10; i++) {
+			dp[1][i] = 1L;
+		}
+		long result = 0;
+		for(int i = 1; i <= 9; i++) {
+			result += recur(N, i);
+		}
+		System.out.println(result % MOD);
+	}
+
+	private static long recur(int digit, int value) {
+		if(digit == 1) {
+			return dp[digit][value];
+		}
+		if(dp[digit][value] == null) {
+			if(value == 0) {
+				dp[digit][value] = recur(digit - 1, 1);
+			}
+			else if(value == 9) {
+				dp[digit][value] = recur(digit - 1, 8);
+			}
+			else {
+				dp[digit][value] = recur(digit - 1, value - 1) + recur(digit - 1, value + 1);
+			}
+		}
+		return dp[digit][value] % MOD;
+	}
+}
