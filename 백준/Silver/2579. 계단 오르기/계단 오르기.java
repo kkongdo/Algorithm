633126d1f9c7c65035e72a90bd36e5bf6ed83f22
Dohyeon Kong <kkong_do@naver.com>
2024-01-11 14:21:42 +0900

    [Silver III] Title: 계단 오르기, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -3,11 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static Integer[] dp; //메모이제이션를 하기 위한 배열
-	static int nums[]; //입력한 값을 넣기 위한 배열
+	static Integer[] dp; 
+	static int nums[]; 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine()); 
 		dp = new Integer[N + 1];
 		nums = new int[N + 1];
 		
@@ -17,7 +17,7 @@ public class Main {
 		dp[0] = nums[0];
 		dp[1] = nums[1];
 		
-		if(N >=2) {
+		if(N >= 2) {
 			dp[2] = nums[1] + nums[2];
 		}
 		System.out.println(find(N));
