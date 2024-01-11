f03609d7728ffbb997f63f1289685a68db1de684
Dohyeon Kong <kkong_do@naver.com>
2024-01-11 16:36:32 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 200 ms, Memory: 22300 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -4,50 +4,53 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int[] nums;
 	static Integer[] dp1;
 	static Integer[] dp2;
-	static int[] seq;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N= Integer.parseInt(br.readLine());
-		int max = -1;
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		nums = new int[N];
 		dp1 = new Integer[N];
 		dp2 = new Integer[N];
-		seq = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < N; i++) {
-			seq[i] = Integer.parseInt(st.nextToken());
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
 		for(int i = 0; i < N; i++) {
 			LIS(i);
 			LDS(i);
 		}
+		int max = -1;
 		for(int i = 0; i < N; i++) {
-			max = Math.max(dp1[i] + dp2[i], max);
+			max = Math.max(dp1[i] +dp2[i], max);
 		}
 		System.out.println(max - 1);
 	}
-	private static int LIS(int N) {
-		if(dp1[N] == null) {
-			dp1[N] = 1;
-			for(int i = N - 1; i >= 0; i--) {
-				if(seq[i] < seq[N]) {
-					dp1[N] = Math.max(dp1[N], LIS(i) + 1);
+
+	private static int LDS(int n) {
+		if(dp2[n] == null) {
+			dp2[n] = 1;
+			for(int i = n + 1; i < dp2.length; i++) {
+				if(nums[n] > nums[i]) {
+					dp2[n] = Math.max(dp2[n], LDS(i) + 1);
 				}
 			}
 		}
-		return dp1[N];
+		return dp2[n];
+		
 	}
-	private static int LDS(int N) {
-		if(dp2[N] == null) {
-			dp2[N] = 1;
-			for(int i = N + 1; i < dp2.length; i++) {
-				if(seq[i] < seq[N]) {
-					dp2[N] = Math.max(dp2[N], LDS(i) + 1);
+
+	private static int LIS(int n) {
+		if(dp1[n] == null) {
+			dp1[n] = 1;
+			for(int i = n - 1; i >= 0; i--) {
+				if(nums[n] > nums[i]) {
+					dp1[n] = Math.max(dp1[n], LIS(i) + 1);
 				}
 			}
 		}
-		return dp2[N];
+		return dp1[n];
 	}
 }
