d1160498d23692074c88208ca5c600c05269826e
Dohyeon Kong <kkong_do@naver.com>
2024-01-13 13:40:33 +0900

    [Gold V] Title: 전깃줄, Time: 128 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -7,19 +7,19 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static Integer[] dp;
-	static int[][] wire;
+	static int[][] line;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		dp = new Integer[N];
-		wire = new int[N][2];
+		line = new int[N][2];
 		StringTokenizer st;
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			wire[i][0] = Integer.parseInt(st.nextToken());
-			wire[i][1] = Integer.parseInt(st.nextToken());
+			line[i][0] = Integer.parseInt(st.nextToken());
+			line[i][1] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(wire, new Comparator<int[]>() {
+		Arrays.sort(line, new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
 				return o1[0] - o2[0];
@@ -28,16 +28,16 @@ public class Main {
 		});
 		int max = 0;
 		for(int i = 0; i < N; i++) {
-			max = Math.max(recur(i), max);
+			max = Math.max(find(i), max);
 		}
 		System.out.println(N - max);
 	}
-	private static int recur(int num) {
+	private static int find(int num) {
 		if(dp[num] == null) {
 			dp[num] = 1;
 			for(int i = num + 1; i < dp.length; i++) {
-				if(wire[num][1] < wire[i][1])
-					dp[num] = Math.max(dp[num], recur(i) + 1);
+				if(line[num][1] < line[i][1])
+					dp[num] = Math.max(dp[num], find(i) + 1);
 			}
 		}
 		return dp[num];
