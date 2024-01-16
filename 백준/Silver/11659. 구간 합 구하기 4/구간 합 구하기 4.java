0dd53ef9ce0498cf80d61e0b7ecc6b29329388b1
Dohyeon Kong <kkong_do@naver.com>
2024-01-16 14:26:33 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1668 ms, Memory: 62656 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -5,25 +5,32 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		
-		int N=Integer.parseInt(st.nextToken());
-		int M=Integer.parseInt(st.nextToken());
-		int[] nums=new int[N+1];
-		
-		st=new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++) {
-			nums[i]=nums[i-1]+Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] nums = new int[N + 1];
+		int[] sums = new int[N + 1];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		for(int k=0; k<M; k++) {
-			st=new StringTokenizer(br.readLine());
-			int i=Integer.parseInt(st.nextToken());
-			int j=Integer.parseInt(st.nextToken());
-			int sum=nums[j]-nums[i-1];
-			
-			System.out.println(sum);
+		for(int i = 1; i <= N; i++) {
+			sums[i] = nums[i] + sums[i - 1];
+		}
+		while(M --> 0) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			if(i == 1) {
+				System.out.println(sums[j]);
+			}
+			else if(i == j) {
+				System.out.println(nums[i]);
+			}
+			else {
+				System.out.println(sums[j] - sums[i - 1]);
+			}
 		}
 	}
 }
