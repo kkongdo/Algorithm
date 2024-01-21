cdc6ba7bfbb75f91e7a2b6bd2132803f7421ff14
Dohyeon Kong <kkong_do@naver.com>
2024-01-21 14:35:15 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,32 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int N=sc.nextInt();
-		int[] nums=new int[N];
-		int[] result=new int[N];
-		int ans=0;
-		
-		for(int i=0; i<N; i++) {
-			nums[i]=sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N= Integer.parseInt(br.readLine());
+		int[] lines = new int[N + 1];
+		int[] sums = new int[N + 1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 1; i <= N; i++) {
+			lines[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(nums);
-		
-		for(int i=0; i<N; i++) {
-			int sum=0;
-			for(int j=0; j<=i; j++) {
-				sum+=nums[j];
-			}
-			result[i]=sum;
+		Arrays.sort(lines);
+		int sum = 0;
+		for(int i = 1; i <= N; i++) {
+			sums[i] = sums[i - 1] + lines[i]; 
+			sum += sums[i]; 
 		}
-		for(int i=0; i<N; i++) {
-			ans+=result[i];
-		}
-		
-		System.out.println(ans);
-		
-		sc.close();
+		System.out.println(sum);
 	}
 }
