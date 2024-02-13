d4efa465db99a9f78a1e83d93c51b72d3c77a842
Dohyeon Kong <kkong_do@naver.com>
2024-02-13 15:51:41 +0900

    [Silver II] Title: 나무 자르기, Time: 520 ms, Memory: 119192 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,7 +19,6 @@ public class Main {
 				max = nums[i];
 			}
 		}
-		Arrays.sort(nums);
 		System.out.println(uppperBound(nums, max, M));
 	}
 
