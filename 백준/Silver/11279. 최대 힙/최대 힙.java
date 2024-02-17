c5c548aa9020243eb531884a3c09e3f98bc395c6
Dohyeon Kong <kkong_do@naver.com>
2024-02-17 15:20:01 +0900

    [Silver II] Title: 최대 힙, Time: 1072 ms, Memory: 31976 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> queue = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+		int num = Integer.parseInt(br.readLine());
+		for (int i = 0; i < num; i++) {
+			int input = Integer.parseInt(br.readLine());
+			if(input == 0) {
+				if(queue.isEmpty()) {
+					System.out.println(0);
+				}
+				else {
+					System.out.println(queue.poll()); 
+				}
+			}
+			else {
+				queue.add(input);
+			}
+		}
+	}
+}
