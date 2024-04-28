e6a7cc14d427160c4755297955e590bc8fc7dd79
Dohyeon Kong <kkongdoes@naver.com>
2024-04-28 15:12:28 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for(String[] cloth : clothes){
+            if(!map.containsKey(cloth[1])){
+                map.put(cloth[1], 1);
+            }
+            else{
+                map.put(cloth[1], map.get(cloth[1]) + 1);
+            }
+        }
+        for(Integer value : map.values()){
+            answer *= value + 1;
+        }
+        return answer - 1;
+    }
+}
