0f167a8c129ceaae22827ee68eb27449cc90056b
Dohyeon Kong <kkongdoes@naver.com>
2024-05-06 15:14:17 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,27 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-//         int[] answer = new int[commands.length];
-        
-//         for(int i = 0; i < commands.length; i++){
-//             ArrayList<Integer> tmp = new ArrayList<Integer>();
-//             for(int j = commands[i][0] - 1; j < commands[i][1]; j++){
-//                 tmp.add(array[j]);
-//             }
-//             Collections.sort(tmp);
-//             answer[i] = tmp.get(commands[i][2] -1);
-//         }
-//         return answer;
         int[] answer = new int[commands.length];
-        for(int i = 0; i < answer.length; i++){
-            int[] command = commands[i];
-            int from = command[0] - 1;
-            int to = command[1];
-            int k = command[2] - 1;
-            
-            int[] sub = Arrays.copyOfRange(array, from, to);
-            Arrays.sort(sub);
-            answer[i] = sub[k];
+        for(int i = 0; i < commands.length; i++){
+            int[] nums = new int[commands[i][1] - commands[i][0] + 1];
+            int count = 0;
+            for(int j = commands[i][0] - 1; j < commands[i][1]; j++){
+                nums[count++] = array[j];
+            }
+            Arrays.sort(nums);
+            answer[i] = nums[commands[i][2] - 1];
         }
         return answer;
     }
