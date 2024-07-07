849ea7e8a45feeac302ce6849ada78c129ce0d8d
Dohyeon Kong <kkongdoes@naver.com>
2024-07-07 15:41:50 +0900

    [Gold III] Title: 오등큰수, Time: 960 ms, Memory: 173580 KB -BaekjoonHub

---
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		final int MAX = 1_000_001;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+	
+		int[] arr = new int[N];
+		int[] count = new int[MAX];
+		int[] answer = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			count[arr[i]]++;
+		}
+		NGF(N, arr, count, answer);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			sb.append(answer[i] + " ");
+		}
+		System.out.println(sb.toString());
+		
+	}
+
+	private static void NGF(int N, int[] arr, int[] count, int[] answer) {
+		Stack<Integer> stack = new Stack<Integer>();
+		for(int i = 0; i < N; i++) {
+			while(!stack.isEmpty() && count[arr[i]] > count[arr[stack.peek()]]) {
+				answer[stack.pop()] = arr[i];
+			}
+			stack.push(i);
+		}
+		while(!stack.isEmpty()) {
+			answer[stack.pop()] = -1;
+		}
+	}
+}
