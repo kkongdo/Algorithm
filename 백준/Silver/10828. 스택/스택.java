5487834b8ed6715c4e085c8fe055875a6788f87f
Dohyeon Kong <kkongdoes@naver.com>
2024-07-07 16:13:08 +0900

    [Silver IV] Title: 스택, Time: 204 ms, Memory: 18748 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,46 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.EmptyStackException;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int N = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<Integer>();
-		StringTokenizer st;
+		Stack<String> stack = new Stack<String>();
+
 		StringBuilder sb = new StringBuilder();
-		
-		for(int i = 0; i < N; i++) {
+		StringTokenizer st;
+
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			String str = st.nextToken();
-			if(str.equals("push")){
-				stack.push(Integer.parseInt(st.nextToken()));
-			}
-			else if(str.equals("top")){
-				if(!stack.isEmpty()) {
-					sb.append(stack.peek()).append("\n");
-				}
-				else {
-					sb.append("-1").append("\n");
-				}
-			}
-			else if(str.equals("size")) {
-				sb.append(stack.size()).append("\n");
-			}
-			else if(str.equals("empty")) {
-				if(stack.isEmpty()) {
-					sb.append("1").append("\n");
-				}else {
-					sb.append("0").append("\n");
-				}
-			}
-			else if(str.equals("pop")) {
-				if(stack.isEmpty()) {
-					sb.append("-1").append("\n");
-				}else {
-					sb.append(stack.pop()).append("\n");
+			String input = st.nextToken();
+			if (input.equals("push")) {
+				stack.push(st.nextToken());
+			} else {
+				switch (input) {
+				case "pop":
+					try {
+						sb.append(stack.pop()).append("\n");
+					} catch (EmptyStackException e) {
+						sb.append("-1").append("\n");
+					}
+					break;
+				case "top":
+					try {
+						sb.append(stack.peek()).append("\n");
+					} catch (EmptyStackException e) {
+						sb.append("-1").append("\n");
+					}
+					break;
+				case "empty":
+					if (stack.isEmpty()) {
+						sb.append(1 + "\n");
+					} else {
+						sb.append(0 + "\n");
+					}
+					break;
+				case "size":
+					sb.append(stack.size() + "\n");
+					break;
 				}
 			}
 		}
