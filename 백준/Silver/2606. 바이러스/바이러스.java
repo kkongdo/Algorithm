231239b5352c3cc3ab65f2be46de89912d813c43
Dohyeon Kong <kkongdoes@naver.com>
2024-08-19 11:18:40 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	static boolean[] visited;
+	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+	static int result;
+	static int count = 1;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int computer = Integer.parseInt(br.readLine());
+		int network = Integer.parseInt(br.readLine());
+		visited = new boolean[computer + 1];
+		
+		for(int i = 0; i <= computer; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+		
+		for(int i = 0; i < network; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int num1 = Integer.parseInt(st.nextToken());
+			int num2 = Integer.parseInt(st.nextToken());
+			
+			graph.get(num1).add(num2);
+			graph.get(num2).add(num1);
+		}
+		DFS(1);
+		System.out.println(result);
+	}
+	private static void DFS(int i) {
+		visited[i] = true;
+		for(Integer num : graph.get(i)) {
+			if(!visited[num]) {
+				result = count++;
+				DFS(num);
+			}
+		}
+	}
+}
