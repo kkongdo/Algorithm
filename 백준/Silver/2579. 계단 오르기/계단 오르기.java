fc051ce3325a31ff95526f8abd671ccb78baf82a
Dohyeon Kong <kkongdoes@naver.com>
2024-11-09 15:56:14 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -3,29 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static Integer[] dp; 
-	static int nums[]; 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine()); 
-		dp = new Integer[N + 1];
-		nums = new int[N + 1];
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N + 1];
+		int[] dp = new int[N + 1];
 		
 		for(int i = 1; i <= N; i++) {
 			nums[i] = Integer.parseInt(br.readLine());
 		}
-		dp[0] = nums[0];
-		dp[1] = nums[1];
 		
-		if(N >= 2) {
+		dp[1] = nums[1];
+		if(N > 1) {
 			dp[2] = nums[1] + nums[2];
 		}
-		System.out.println(find(N));
-	}
-	private static int find(int n) {
-		if(dp[n] == null) {
-			dp[n] = Math.max(find(n - 2), find(n - 3) + nums[n - 1]) + nums[n];
+		if(N > 2){
+			dp[3] = Math.max(nums[1] + nums[3], nums[2] + nums[3]);
+			
 		}
-		return dp[n];
+		for(int i = 4; i <= N; i++) {
+			dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 3] + nums[i - 1] + nums[i]);
+		}
+		System.out.println(dp[N]);
 	}
 }
