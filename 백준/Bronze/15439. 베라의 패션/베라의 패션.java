fa42f6cf7dce99099942e2ea73484a1d819517fc
Dohyeon Kong <kkongdoes@naver.com>
2025-07-13 22:49:12 +0900

    [Bronze IV] Title: 베라의 패션, Time: 96 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/15439. 베라의 패션/베라의 패션.java
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		
 		System.out.println(N * (N - 1));
 	}
 }
